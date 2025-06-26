643be68e89486d2839a1f889531be9e7548ecd04
rainingfrog <32581794+rainingfrog@users.noreply.github.com>
2025-06-26 08:22:11 -0400

    Add files via upload
    ....

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\dlman\\Documents\\HomeMin_App\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\dlman\\untitled" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\dlman\\Documents\\HomeMin_App\\flutter"
+  "PROJECT_DIR=C:\\Users\\dlman\\untitled"
+  "FLUTTER_ROOT=C:\\Users\\dlman\\Documents\\HomeMin_App\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\dlman\\untitled\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\dlman\\untitled"
+  "FLUTTER_TARGET=C:\\Users\\dlman\\untitled\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzIuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NmZiYTI0NDdlOQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049OGNkMTllNTA5ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\dlman\\untitled\\.dart_tool\\package_config.json"
+)
